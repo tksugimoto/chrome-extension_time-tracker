a048e8d2028851b8dbb85b2c04397023c7df125a
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2023-12-03 19:20:25 +0900

    chore: enable eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+	},
+	plugins: [
+		'html',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	extends: [
+		'eslint:recommended',
+	],
+	overrides: [{
+		files: ['.eslintrc.js'],
+		env: {
+			node: true,
+		},
+	}],
+	rules: {
+		quotes: ['warn', 'single', {
+			avoidEscape: true,
+		}],
+		'comma-dangle': ['error', 'always-multiline'],
+		semi: 'error',
+		'no-var': 'error',
+	},
+};
