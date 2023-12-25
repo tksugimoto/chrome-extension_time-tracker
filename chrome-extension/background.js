d67a9ea5190cbaf2c8a0b3de9b969b25a801969f
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2023-12-25 20:00:40 +0900

    feat: キーボードショートカットで開くorフォーカスする機能を追加

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -17,3 +17,21 @@ chrome.contextMenus.onClicked.addListener(info => {
 		});
 	}
 });
+
+chrome.commands.onCommand.addListener(command => {
+	if (command === 'open_or_focus') {
+		const url = chrome.runtime.getURL(chrome.runtime.getManifest().action.default_popup);
+		chrome.tabs.query({
+			url, // "tabs" 権限が必要。自拡張のURLでも必要。
+		}).then(([tab]) => {
+			if (!tab) return chrome.tabs.create({url});
+
+			chrome.windows.update(tab.windowId, {
+				focused: true,
+			});
+			chrome.tabs.update(tab.id, {
+				active: true,
+			});
+		});
+	}
+});
