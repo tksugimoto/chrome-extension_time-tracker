edeb28e6f8880d6a35f95bc72e96c2be6a41c587
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2024-04-14 18:48:17 +0900

    feat: ToDoグループの順番変更機能を追加

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -9,6 +9,17 @@ const {
 	useMemo,
 } = React;
 
+
+/**
+ *
+ * @param {any[]} array
+ * @param {number} index1
+ * @param {number} index2
+ */
+const swap = (array, index1, index2) => {
+	[array[index1], array[index2]] = [array[index2], array[index1]];
+};
+
 const domParser = new DOMParser();
 
 /**
@@ -950,6 +961,22 @@ const App = () => {
 												}
 											},
 										}, '削除'),
+										createElement('button', {
+											disabled: groupIndex === 0,
+											onClick: () => {
+												// FIXME: mutableをやめる
+												swap(todoGroups, groupIndex, groupIndex - 1);
+												saveTodoGroup();
+											},
+										}, '上へ移動'),
+										createElement('button', {
+											disabled: groupIndex === todoGroups.length - 1,
+											onClick: () => {
+												// FIXME: mutableをやめる
+												swap(todoGroups, groupIndex, groupIndex + 1);
+												saveTodoGroup();
+											},
+										}, '下へ移動'),
 									),
 								),
 								createList(groupdMap.get(group) ?? []),
